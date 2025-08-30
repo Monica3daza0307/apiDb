76776f5d1acd0707917660c3aa4241f1444fe5ba
406 - Comunicaciones <406@laboratorios.usbbog.edu.co>
2025-08-29 19:22:21 -0500

    adding

--- controllers/band_controllers.py
+++ controllers/band_controllers.py
@@ -3,11 +3,12 @@ form flask import Blueprint
 band_bp =Blueprint('band_bp',__name__)
 
 @band_bp('/bands', methods=['GET'])
+    print("Hola Moni")
 
 @band_bp('/bands/<int:band_id>', methods=['GET'])
-
+    print("Hola Moni")
 @band_bp('/bands', methods=['POST'])
-
+    print("Hola Moni")
 @band_bp('/bands/<int:band_id>', methods=['PUT'])
-
+    print("Hola Moni")
 @band_bp('/bands/<int:band_id>', methods=['DELETE'])
